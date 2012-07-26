876cc00f526d0c3da1b42ab96de1ef352f226660
Matt <code@zonbi.org>
2012-07-26 16:01:00 +0200

    maths works. added file output. just need to finish up the graphing

--- iostatparse.py
+++ iostatparse.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python
 
-# take iostat output and graph it
+# take iostat output and parse it into something pretty
+
+# -f = file you want to parse
+# -c = column you want to investigate
+# -s = how many samples you want to parse
+# -g = option will create pretty graphs
+# -o = option will output to a text file
+
+# Eg. ./iostatparse.py -f c:\iostat.raw -c 21 -s 150 -o c:\iostat_parsed.txt -g c:\iostat_graph.png
+
+# note that this script expects raw iostat output for input. clean it up with: 
+# 		cat iostat.txt |  grep -v -e "\(cpu\|id\)" > iostat.raw
+# yes, I will add code to clean up files, but in the mean time...
+
+# code@zonbi.org
 
 import argparse
 import sys
 
-def main():
+def main(): # get this party started
 
 	#basic menu system
 	parser = argparse.ArgumentParser(description='parse and graph iostat output', usage='%(prog) -f file -c column')
 	parser.add_argument('-f', dest='file', help='file to parse')
-	parser.add_argument('-c', dest='column', help='column to parse')
-	parser.add_argument('-t', dest='time', help='time slice to parse')
+	parser.add_argument('-c', dest='column', help='column to investigate')
+	parser.add_argument('-s', dest='samples', help='how many samples to parse')
+	parser.add_argument('-o', dest='output', help='write results to file')
 	parser.add_argument('-g', action='store_true', dest='graph', help='generate pretty graphs')
 	parser.add_argument('--version', '-v', action='version', version='%(prog)s 0.1')
 	args = parser.parse_args()
@@ -21,47 +36,64 @@ def main():
 		parser.print_help()
 		sys.exit(1)
 
+	# better way to do this ?
 	global arg_file
 	global arg_column
-	global arg_time
+	global arg_samples
 	global arg_graph
+	global arg_output
 	arg_file = args.file
 	arg_column = args.column
-	arg_time = args.time
+	arg_samples = args.samples
+	arg_output = args.output
 	arg_graph = args.graph
 	line_num()
 	file_parser()
 
-def line_num():
+def line_num(): # count the number of lines we're parsing
 	global num_lines
 	num_lines = sum(1 for line in open(arg_file, 'r'))
 	
-def array_calc():
+def array_calc(): # do magic on our counters to get percentages
 
 	print ">> doing magical math stuff"
 	print ""
+	global to9perc
 	to9perc = float(to9counter) * 100 / int(num_lines)
 	print "io load 0-9% " + str(to9counter) + " times out of " + str(num_lines) + " (" + str(to9perc) + "%)"
-	to19perc = float(to9counter) * 100 / int(num_lines)
+	global to19perc
+	to19perc = float(to19counter) * 100 / int(num_lines)
 	print "io load 10-19% " + str(to19counter)  + " times out of " + str(num_lines) + " (" + str(to19perc) + "%)"
-	to29perc = float(to9counter) * 100 / int(num_lines)
+	global to29perc
+	to29perc = float(to29counter) * 100 / int(num_lines)
 	print "io load 20-29% " + str(to29counter)  + " times out of " + str(num_lines) + " (" + str(to29perc) + "%)"
-	to39perc = float(to9counter) * 100 / int(num_lines)
+	global to39perc
+	to39perc = float(to39counter) * 100 / int(num_lines)
 	print "io load 30-39% " + str(to39counter)  + " times out of " + str(num_lines) + " (" + str(to39perc) + "%)"
-	to49perc = float(to9counter) * 100 / int(num_lines)
+	global to49perc
+	to49perc = float(to49counter) * 100 / int(num_lines)
 	print "io load 40-49% " + str(to49counter)  + " times out of " + str(num_lines) + " (" + str(to49perc) + "%)"
-	to59perc = float(to9counter) * 100 / int(num_lines)
+	global to59perc
+	to59perc = float(to59counter) * 100 / int(num_lines)
 	print "io load 50-59% " + str(to59counter)  + " times out of " + str(num_lines) + " (" + str(to59perc) + "%)"
-	to69perc = float(to9counter) * 100 / int(num_lines)
+	global to69perc
+	to69perc = float(to69counter) * 100 / int(num_lines)
 	print "io load 60-69% " + str(to69counter)  + " times out of " + str(num_lines) + " (" + str(to69perc) + "%)"
-	to79perc = float(to9counter) * 100 / int(num_lines)
+	global to79perc
+	to79perc = float(to79counter) * 100 / int(num_lines)
 	print "io load 70-79% " + str(to79counter)  + " times out of " + str(num_lines) + " (" + str(to79perc) + "%)"
-	to89perc = float(to9counter) * 100 / int(num_lines)
+	global to89perc
+	to89perc = float(to89counter) * 100 / int(num_lines)
 	print "io load 80-89% " + str(to89counter)  + " times out of " + str(num_lines) + " (" + str(to89perc) + "%)"
-	to100perc = float(to9counter) * 100 / int(num_lines)
+	global to100perc
+	to100perc = float(to100counter) * 100 / int(num_lines)
 	print "io load 90-100% " + str(to100counter)+ " times out of " + str(num_lines) + " (" + str(to100perc) + "%)"
 	print ""
-	if arg_graph:
+	if arg_output:
+		print ">> GOTO: OUTPUT"
+		print "----------------------------------------------"
+		write_output()
+	elif arg_graph:
 		print ">> GOTO: GRAPHS"
 		print "----------------------------------------------"
 		gen_graph()
@@ -70,7 +102,47 @@ def array_calc():
 		print "----------------------------------------------"
 		sys.exit(">> thanks for playing.")
 
-def gen_graph():
+def write_output():
+	print ">> writing results to file"
+	print ""
+	fileout = open(arg_output, 'a')
+	header = "output from iostat: " + arg_file + "\n"
+	body0 = "\nio load 0-9% " + str(to9counter) + " times out of " + str(num_lines) + " (" + str(to9perc) + "%)\n"
+	body1 = "io load 10-19% " + str(to19counter)  + " times out of " + str(num_lines) + " (" + str(to19perc) + "%)\n"
+	body2 = "io load 20-29% " + str(to29counter)  + " times out of " + str(num_lines) + " (" + str(to29perc) + "%)\n"
+	body3 = "io load 30-39% " + str(to39counter)  + " times out of " + str(num_lines) + " (" + str(to39perc) + "%)\n"
+	body4 = "io load 40-49% " + str(to49counter)  + " times out of " + str(num_lines) + " (" + str(to49perc) + "%)\n"
+	body5 = "io load 50-59% " + str(to59counter)  + " times out of " + str(num_lines) + " (" + str(to59perc) + "%)\n"
+	body6 = "io load 60-69% " + str(to69counter)  + " times out of " + str(num_lines) + " (" + str(to69perc) + "%)\n"
+	body7 = "io load 70-79% " + str(to79counter)  + " times out of " + str(num_lines) + " (" + str(to79perc) + "%)\n"
+	body8 = "io load 80-89% " + str(to89counter)  + " times out of " + str(num_lines) + " (" + str(to89perc) + "%)\n"
+	body9 = "io load 90-100% " + str(to100counter)+ " times out of " + str(num_lines) + " (" + str(to100perc) + "%)\n"
+	footer = "\nmessage ends\n"
+
+	fileout.write(header)
+	fileout.write(body0) # I'm pretty sure this can be improved on :D
+	fileout.write(body1)
+	fileout.write(body2)
+	fileout.write(body3)
+	fileout.write(body4)
+	fileout.write(body5)
+	fileout.write(body6)
+	fileout.write(body7)
+	fileout.write(body8)
+	fileout.write(body9)
+	fileout.write(footer)
+	fileout.close()
+
+	if arg_graph:
+		print ">> GOTO: GRAPHS"
+		print "----------------------------------------------"
+		gen_graph()
+	else: 
+		print ">> END"
+		print "----------------------------------------------"
+		sys.exit(">> thanks for playing.")
+
+def gen_graph(): # create pretty graphs
 
 	print ">> generating graphs"
 	# generate graphs here
@@ -80,12 +152,12 @@ def gen_graph():
 	sys.exit(">> thanks for playing.")
 
 
-def file_parser():
+def file_parser(): # parse our file (error checking is for wimps)
 	print "----------------------------------------------"
 	print ">> BEGIN: "
 	print "parsing: " + arg_file
 	print "lines: " + str(num_lines)
-	print "time: " + str(arg_time)
+	print "samples: " + str(arg_samples)
 	print "column: " + str(arg_column) 
 	print ""
 	print ">> GOTO: PARSER"
@@ -120,44 +192,24 @@ def file_parser():
 		point = int((line.split()[int(arg_column)]))
 
 		if (point >= 0) and (point <= 9):
-			# place the value into an array
-			#print "1-9: " +  str(point)
 			to9counter = to9counter + 1
 		elif (point >= 10) and (point <= 19):
-			# place the value into an array
-			#print "10 - 19: " + str(point)
 			to19counter = to19counter + 1
 		elif (point >= 20) and (point <= 29):
-			# place the value into an array
-			#print "20-29: " + str(point)
 			to29counter = to29counter + 1
 		elif (point >= 30) and (point <= 39):
-			# place the value into an array
-			#print "30-39: " + str(point)
 			to39counter = to39counter + 1
 		elif (point >= 40) and (point <= 49):
-			# place the value into an array
-			#print "40-49: " + str(point)
 			to49counter = to49counter + 1
 		elif (point >= 50) and (point <= 59):
-			# place the value into an array
-			#print "50-59: " + str(point)
 			to59counter = to59counter + 1
 		elif (point >= 50) and (point <= 69):
-			# place the value into an array
-			#print "60-69: " + str(point)
 			to69counter = to69counter + 1
 		elif (point >= 70) and (point <= 79):
-			# place the value into an array
-			#print "70-79: " + str(point)
 			to79counter = to79counter + 1			
 		elif (point >= 80) and (point <= 89):
-			# place the value into an array
-			#print "80-89: " + str(point)
 			to89counter = to89counter + 1		
 		else:
-			# place the value into an array
-			#print "90-100: " + str(point) 
 			to100counter = to100counter + 1	
 
 	print ""
