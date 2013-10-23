9e2dea28bc9bad98ce4458b1bae92bc137705472
Matt <code@zonbi.org>
2013-10-23 15:59:49 +0200

    epoch to date working

--- converttime.py
+++ converttime.py
@@ -8,24 +8,36 @@ import argparse
 import sys
 import time
 
-def convert_time(atime):
+def convert_epoch(aepoch):
 	"""convert our time to something else"""
-	print "time given: " + atime
-	epoch = time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime(atime))
-	print epoch
+	print "time given: " + aepoch
+	epoch = time.strftime("%a %d %b %Y %H:%M:%S +0000", time.gmtime(float(aepoch)))
+	print "converted time: " + epoch
+
+
+def convert_date(adate):
+	"""convert regular old dates back to epoch"""
+	print "date given: " + adate
+	# stuff
+	print "epoch time for date: " 
 
 def __main__():
 
     parser = argparse.ArgumentParser(description='basic time converter', usage='%(prog)s')
-    parser.add_argument('--time', '-t', dest='time', help='time to convert')
+    parser.add_argument('--epoch', '-e', dest='epoch', help='convert from epoch')
+    parser.add_argument('--date', '-d', dest='date', help='convert from regular old dates')
     parser.add_argument('--version', '-v', action='version', version='%(prog)s 0.1')
     args = parser.parse_args()
-    atime = args.time
+    aepoch = args.epoch
+    adate = args.date
 
-    if not args.time:
+    if not args.epoch and not args.date:
         sys.exit(parser.print_help())
 
-    convert_time(atime)
+    if args.epoch:
+    	convert_epoch(aepoch)
+    else:
+    	convert_date(adate)
 
 
 if __name__ == '__main__':
