1291785316864cce2f6a87867f8b36944b371125
Matt <code@zonbi.org>
2014-01-10 01:50:58 +0100

    Header checks working...kinda

--- redirtrace.py
+++ redirtrace.py
@@ -5,13 +5,57 @@ import argparse
 import sys
 import requests
 
-def checkurl(aurl):
+
+def parsebody(bodydata):
+    """Parsing body for the good stuff"""
+    print "\nParsing body"""
+    #print bodydata
+    
+
+
+def parseheaders(headerdata):
+    """Parsing headers for the good stuff"""
+    print "Parsing headers"""
+    #print headerdata
+    server = headerdata['server']
+    print "`~> Server: " + server
+
+    try:
+        location = headerdata['Location']
+        if (location) == None:
+            parsebody(body)
+        else:
+            print " `~> Header Redirect: " + location
+            print "\n`~> New request: " + location + "\n"
+            headers, body = makerequest(location)
+            parseheaders(headers)
+    except KeyError:
+        print "No location redirects in headers :("
+
+
+def makerequest(requrl):
+    """Make the HTTP request"""
+    print "Request: " + str(requrl)
+    req = requests.head(requrl)
+    headers = req.headers
+    bodydata = req.text
+    status_code = req.status_code
+    history = req.history
+    print "Status code: " + str(status_code)
+    if history == []:
+        pass
+    else: 
+        print "Redirect " + str(history)
+    if status_code == 404:
+        sys.exit("Erroring out: We got a 404")
+
+    return (headers, bodydata)
+
+
+def checkurl(url):
     """Check a URL for redirects and the like"""
-    print "Checking: " + aurl
-    req = requests.get(aurl, allow_redirects=False)
-    sc = req.status_code
-    if sc == 404:
-        sys.exit("We got a 404")
+    headers, body = makerequest(url)
+    parseheaders(headers)
 
 
 def __main__():
@@ -20,11 +64,13 @@ def __main__():
     parser.add_argument('--url', '-u', dest='url', help='url to check')
     parser.add_argument('--version', '-v', action='version', version='%(prog)s 0.1')
     args = parser.parse_args()
+    global aurl
     aurl = args.url
 
     if not args.url:
         sys.exit(parser.print_help())
 
+    print "Checking: " + str(aurl) + "\n"
     checkurl(aurl)
 
 
