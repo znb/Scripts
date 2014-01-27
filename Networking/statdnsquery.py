16eb4faba4c8c69cd61131f22232ef860b3ea328
Matt <code@zonbi.org>
2014-01-27 14:43:59 +0100

    nipples

--- Networking/statdnsquery.py
+++ Networking/statdnsquery.py
@@ -9,7 +9,8 @@ import requests
 def dnsquery(aqtype, adomain):
     """check statdns.com for our domain"""
 
-    print "Checking domain: " + str(adomain) + " for DNS record type: " + str(aqtype)
+    print "Checking domain: " + str(adomain) + " for DNS record type: " + str(aqtype).upper()
+    print " >>"
     fullurl = "http://api.statdns.com/" + adomain + "/" + aqtype
     resp = requests.get(fullurl)
 
@@ -18,6 +19,9 @@ def dnsquery(aqtype, adomain):
     if resp_code == 400:
         print "Error"
     print report
+    results = report.get("answer", {})
+    for result in results:
+        print result
 
     return 0
 
@@ -38,22 +42,38 @@ def reversequery(adomain):
     sys.exit()
 
 
+def querylist():
+    """Print out a list of valid query types"""
+    print "Valid query types: ", 
+    for query in ['a', 'aaaa', 'cert', 'cname', 'dhcid', 'dlv', 'dname', 'dnskey', 'ds', 
+            'hinfo', 'hip', 'ipseckey', 'kx', 'loc', 'mx', 'naptr', 'ns', 'nsec', 
+            'nsec3', 'nsec3param', 'opt', 'ptr', 'rrsig', 'soa', 'spf', 'srv', 
+            'sshfp', 'tai', 'talink', 'tlsa', 'txt']:
+        print query.upper(),
+
+    sys.exit()
+
+
 def __main__():
     """basic menu system"""
-    parser = argparse.ArgumentParser(description='DNS Query tool', usage='%(prog)s -t type -d domain/hostname')
+    parser = argparse.ArgumentParser(description='DNS Query tool')
     parser.add_argument('--type', '-t', dest='qtype', help='type of query')
     parser.add_argument('--reverse', '-r', dest='reverse', help='reverse PTR records for query')
     parser.add_argument('--domain', '-d', dest='domain', help='domain/hostname we are interested in')
+    parser.add_argument('--list', '-l', dest='querylist', action='store_true', help='list valid query types')
     parser.add_argument('--version', '-v', action='version', version='%(prog)s 0.2')
     args = parser.parse_args()
     aqtype = args.qtype
     adomain = args.domain
     areverse = args.reverse
 
+    #if not (args.qtype and args.domain):
+    #    sys.exit(parser.print_help())
+
     if args.reverse:
         reversequery(areverse)
-    if not (args.qtype and args.domain):
-        sys.exit(parser.print_help())
+    elif args.querylist:
+        querylist()
     else:
         dnsquery(aqtype, adomain)
 
