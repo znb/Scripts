0c75520a0af36c01d07409cf6c998f22dd290dc4
Matt <code@zonbi.org>
2014-02-12 03:49:10 -0800

    Added functions and a dump report feature

--- Security/courier-attacks.sh
+++ Security/courier-attacks.sh
@@ -5,13 +5,10 @@ LOGFILE="/var/log/exim4/rejectlog"
 ATTACKERS="/tmp/.attackers"
 USERS="/tmp/.users"
 REGEX="Incorrect"
+DUMPREPORT="./attacker-report.txt"
 
 
-
-echo "Getting stats"
-cat ${LOGFILE} | grep ${REGEX} | awk -F"[" '{ print $2 }' | awk -F"]" '{ print $1}' | sort| uniq > ${ATTACKERS}
-cat ${LOGFILE} | grep ${REGEX} | awk -F"=" ' { print $2} ' | awk -F")" '{ print $1 }' | sort | uniq > ${USERS}
-
+function attackersummary() {
 echo "Attacker Summary"
 echo "---------------------------------------"
 for x in `cat ${ATTACKERS}` 
@@ -20,11 +17,23 @@ for x in `cat ${ATTACKERS}`
     grep ${REGEX} ${LOGFILE} | grep -e "${x}" | awk -F"=" ' { print $2} ' | awk -F")" '{ print $1 }'  | wc -l 
 done
 echo "---------------------------------------"
+ }
 
-
+function summaryquery() {
 echo -n "Print username summary Y/N? "
 read ANSWER
-if [ $ANSWER == "y" ]; then
+if [ ${ANSWER} == "y" ]; then
+		usersummary
+else
+        dumpreport
+		# Clean up
+		rm ${ATTACKERS} ${USERS}
+		echo "Exiting"
+		exit 0
+fi
+}
+
+function usersummary() {
 		echo "Attacker username summary"
 		echo "---------------------------------------"
 		for x in `cat ${ATTACKERS}` 
@@ -45,11 +54,28 @@ if [ $ANSWER == "y" ]; then
 		  echo "---------------------------------------"
 		 rm /tmp/${x}-attack_summary.txt
 		done
-else
-		# Clean up
-		rm ${ATTACKERS} ${USERS}
-		echo "Exiting"
-		exit 0
-fi
+}
 
+function dumpreport() {
+echo -n "Dump report to file ? Y/N "
+read ANSWER
+if [ ${ANSWER} = "y" ] 
+ then
+  echo "Dumping: ${DUMPREPORT}"
+  usersummary > ${DUMPREPORT}
+else
+ echo "We're done here."
+ exit 0
+fi
+}
+
+echo "Getting stats"
+cat ${LOGFILE} | grep ${REGEX} | awk -F"[" '{ print $2 }' | awk -F"]" '{ print $1}' | sort| uniq > ${ATTACKERS}
+cat ${LOGFILE} | grep ${REGEX} | awk -F"=" ' { print $2} ' | awk -F")" '{ print $1 }' | sort | uniq > ${USERS}
+
+attackersummary
+summaryquery
+dumpreport
+  
+echo "Report complete"
 
