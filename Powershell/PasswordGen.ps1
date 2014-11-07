9b4d43d8974a660c6d1ba8814a5bdb7e3e5526d9
Matt Erasmus <matt.erasmus@ticketmaster.co.uk>
2014-11-07 12:34:08 +0000

    Added a length arguement

--- Powershell/PasswordGen.ps1
+++ Powershell/PasswordGen.ps1
@@ -1,19 +1,24 @@
 # Simple script to generate a decent password and copy to clipboard
 
 param( 
-[int] $len = 15,  # Adjust this if required
+#[int] $len = 15,  # Adjust this if required
+[string] $length = 15,
 [string] $chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_!@#$%^&*()_"
 )
 
-Write-Output "Generating Password with length: $len"
-$bytes = new-object "System.Byte[]" $len
+Write-Output "Generating Password with length: $length"
+if ($length -le "10") {
+Write-Output "[WARNING] Passwords less than 10 caracters are considered weak"
+}
+
+$bytes = new-object "System.Byte[]" $length
 $rnd = new-object System.Security.Cryptography.RNGCryptoServiceProvider
 $rnd.GetBytes($bytes)
 $result = ""
 
-for( $i=0; $i -lt $len; $i++ )
+for( $i=0; $i -lt $length; $i++ )
 {
-$result += $chars[ $bytes[$i] % $chars.Length ]	
+$result += $chars[ $bytes[$i] % $chars.length ]	
 }
 
 # This copies stuff to the clipboard
@@ -24,7 +29,6 @@ $dataObject = New-Object windows.forms.dataobject
 $dataObject.SetData([Windows.Forms.DataFormats]::UnicodeText, $true, $result)
 [Windows.Forms.Clipboard]::SetDataObject($dataObject, $true)
 }
-
 
 To-Clipboard
 Write-Output "`r`nPassword copied to clipboard`r`n"
